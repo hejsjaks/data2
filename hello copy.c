d03c86fa99429df74fb86e8237e998861f04b501
hejsjaks <bmy78000@naver.com>
2025-09-11 12:27:08 +0900

    9월11일강의 수정

--- hello copy.c
+++ hello copy.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-   int a,b,c;
+   int a,b,c,kkk;
 
-   printf("세 징수의 최댓값을 구합니다.\n")
+   printf("세 징수의 최댓값을 구합니다.\n");
    printf("a값:  "); scanf("%d",&a);
    printf("b값:  "); scanf("%d",&b);
    printf("c값:  "); scanf("%d",&c);
-
+   
    int max = a;
    if(b > max) max = b;
    if(c > max) max = c;
