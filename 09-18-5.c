3f522a38c3077542622e8fec4a62c60db21efbf1
hejsjaks <bmy78000@naver.com>
2025-09-18 12:54:16 +0900

    9월18일 강의

--- 09-18-5.c
+++ 09-18-5.c
@@ -2,16 +2,32 @@
 
 int main(void){
     int n;
+    int num = n;
     printf("몇 단 삼각형 입니까?: ");
     scanf("%d", &n);
 
     for (int i = 1; i < n; i++)
     {
-        /* code */ for (int j = 1; j <= i; j++)
+        /* code */ 
+        for (int j = 1; j <= i; j++)
+                putchar(" ");
+        for (int j = 0; j < 2+i; j++)
         putchar("*");
         putchar("\n");
 
+
+        
+
     }
+    for (int i = 0; i < n; i++){
+         putchar(" ");
+        for (int j = n - 1; j > i; j--)
+        putchar(" ");
+        for (int j = 0; j <2* i + 1; j++)
+        putchar("*");
+        putchar("\n");
+    }
+               
     
    
 }
